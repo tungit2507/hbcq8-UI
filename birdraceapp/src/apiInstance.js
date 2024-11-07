d651fc7b078bb319c65c284634110fa030fe16b5
Hoang Thanh Tung <tunghoang25073012@gmail.com>
2024-11-07 18:52:54 +0700

    change api

--- birdraceapp/src/apiInstance.js
+++ birdraceapp/src/apiInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useNavigate } from 'react-router-dom';
 
 const axioInstance = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'http://103.69.84.147:8080/api/v1',
   withCredentials: true,
 });
 
