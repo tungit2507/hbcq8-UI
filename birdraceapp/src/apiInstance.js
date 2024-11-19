345d9aa0ec9d33122cba9c85c249912407e90300
Hoang Thanh Tung <tunghoang25073012@gmail.com>
2024-11-20 04:05:52 +0700

    change IP to localhost

--- birdraceapp/src/apiInstance.js
+++ birdraceapp/src/apiInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axioInstance = axios.create({
-  baseURL: 'http://103.69.84.147:8080/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
   withCredentials: true,
 });
 
