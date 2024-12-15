30ee88a717666ac13adcf28e8aa17dc8055c2406
Hoang Thanh Tung <tunghoang25073012@gmail.com>
2024-12-15 15:07:35 +0700

    hide role of club

--- birdraceapp/src/components/testimonialsSection.jsx
+++ birdraceapp/src/components/testimonialsSection.jsx
@@ -24,32 +24,32 @@ const testimonials = [
 ];
 
 const Testimonials = () => {
-  return (
-    <section className="testimonials-12 testimonials section" id="testimonials">
-      <div className="container section-title" data-aos="fade-up">
-        <h2>THÀNH VIÊN QUẢN TRỊ CỦA CLB</h2>
-      </div>
+  // return (
+  //   <section className="testimonials-12 testimonials section" id="testimonials">
+  //     <div className="container section-title" data-aos="fade-up">
+  //       <h2>THÀNH VIÊN QUẢN TRỊ CỦA CLB</h2>
+  //     </div>
 
-      <div className="testimonial-wrap">
-        <div className="container">
-          <div className="row">
-            {testimonials.map((testimonial, index) => (
-              <div className="col-md-6 mb-4 mb-md-4" key={index}>
-              <div className="testimonial d-flex flex-column align-items-center">
-                <img src={testimonial.image} alt={`Testimonial author ${index + 1}`} />
-                <blockquote style={{ textAlign: 'justify' }}>
-                  <p>{testimonial.quote}</p>
-                </blockquote>
-                <p className="client-name">{testimonial.name}</p>
-              </div>
-            </div>
+  //     <div className="testimonial-wrap">
+  //       <div className="container">
+  //         <div className="row">
+  //           {testimonials.map((testimonial, index) => (
+  //             <div className="col-md-6 mb-4 mb-md-4" key={index}>
+  //             <div className="testimonial d-flex flex-column align-items-center">
+  //               <img src={testimonial.image} alt={`Testimonial author ${index + 1}`} />
+  //               <blockquote style={{ textAlign: 'justify' }}>
+  //                 <p>{testimonial.quote}</p>
+  //               </blockquote>
+  //               <p className="client-name">{testimonial.name}</p>
+  //             </div>
+  //           </div>
             
-            ))}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+  //           ))}
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </section>
+  // );
 };
 
 export default Testimonials;
