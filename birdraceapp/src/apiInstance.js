e45140a0ecd123cfa66b49d3642cb525e7881f45
Hoang Thanh Tung <tunghoang25073012@gmail.com>
2024-12-16 17:49:22 +0700

    change domain

--- birdraceapp/src/apiInstance.js
+++ birdraceapp/src/apiInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axioInstance = axios.create({
-  baseURL: 'http://103.69.84.147:8080/api/v1',
+  baseURL: 'https://clbbcduaq8.com/api/v1',
   withCredentials: true,
 });
 
--- birdraceappadmin/src/api/api.js
+++ birdraceappadmin/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://103.69.84.147:8080',
+  baseURL: 'https://clbbcduaq8.com',
   withCredentials: true,
 });
 
