d355628c273278b80d26a633ca33f240998e8cc7
Hoang Thanh Tung <tunghoang25073012@gmail.com>
2025-01-18 17:58:51 +0700

    change domain

--- birdraceappadmin/src/api/api.js
+++ birdraceappadmin/src/api/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://clbbcduaq8.com/',
   withCredentials: true,
 });
 
